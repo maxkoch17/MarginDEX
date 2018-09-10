7c346f53343f934ad59a232b8724ea53c24e39e3
Kevin <kevinzittlau@gmail.com>
2018-09-10 13:44:49 -0400

    Create Github

---
+++ src/LoginContainer.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+class LoginContainer extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {};
+  }
+
+  render(){
+    this.props;
+    return <div> </div>;
+  }
+
+  componentDidMount(){
+
+  }
+
+  componentWillUnmount(){
+
+  }
+}
